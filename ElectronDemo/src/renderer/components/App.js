695abe4d3684d0053c7cf8d7c274734547e1811e
ruibin.chow <ruibin.chow@qq.com>
2023-05-06 16:03:49 +0800

    [ElectronDemo] Using es6 syntax for react component.

--- ElectronDemo/src/renderer/components/App.js
+++ ElectronDemo/src/renderer/components/App.js
@@ -3,38 +3,54 @@ import '../assets/css/App.css'
 import logger from '../log'
 import { Button, message, notification } from 'antd';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    logger.info("[App]", "App Load.");
 
-function App() {
-  logger.info("[App]", "App Load.");
+    this.state = {
+      title: "Hello, Electron Demo!",
+      detail: "I hope you enjoy using electron demo to start your dev off right!"
+    };
 
-  const [messageApi, messageHolder] = message.useMessage();
-  const info = () => {
-    messageApi.info("Electron Demo");
+    // 为了在回调中使用 `this`，这个绑定是必不可少的
+    this.info = this.info.bind(this);
+    this.openNotification = this.openNotification.bind(this);
+  }
+  
+  info() {
+    message.info("Electron Demo");
     logger.debug("[App]", "Alert.");
+
+    this.setState(state => ({
+      title: 'Hello, Electron And React!',
+    }));
   };
 
-  const [notificationApi, notificationHolder] = notification.useNotification();
-  const openNotification = () => {
-    notificationApi.open({
+  openNotification() {
+    notification.open({
       message: 'Notification Title',
       description:
         'I will never close automatically. This is a purposely very very long description that has many many characters and words.',
       duration: 3.5,
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
     });
   };
 
-  return (
-    <div>
-      <h1>Hello, Electron Demo!</h1>
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.detail}</p>
 
-      <p>I hope you enjoy using electron demo to start your dev off right!</p>
-      {messageHolder}
-      <Button type="primary" onClick={info}>Alert</Button>&nbsp;
-
-      {notificationHolder}
-      <Button type="primary" onClick={openNotification}>Notification</Button>
-    </div>
-  )
+        <Button type="primary" onClick={this.info}>Alert</Button>
+        &nbsp;
+        <Button type="primary" onClick={this.openNotification}>Notification</Button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
