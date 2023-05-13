2974d453d8d75c06d5a855bd9977354f37f413c7
ruibin.chow <ruibin.chow@qq.com>
2023-05-13 20:51:10 +0800

    [QtDemo] Add audio capture and process raw pcm data.

--- QTDemo/src/multimedia/audio_capture.cc
+++ QTDemo/src/multimedia/audio_capture.cc
@@ -8,16 +8,22 @@
 
 #include "audio_capture.h"
 #include <QDebug>
-#include <QMediaFormat>
 #include <QUrl>
 #include <QFileDialog>
 #include "permission.h"
+#include <QDir>
+#include <QStandardPaths>
 
 /*
  https://blog.csdn.net/qq582880551/article/details/123764531
  */
 
 AudioCaputer::AudioCaputer() {
+    qDebug() << "homePath:" << QDir::homePath();
+    QString desktop = QStandardPaths::writableLocation(QStandardPaths::DesktopLocation);
+    qDebug() << "Desktop Path:" << desktop;
+    filePath_.reset(new QString(desktop.append("/record.pcm")));
+    
     Qt::PermissionStatus status = Permission::GetInstance()->CheckMicrophonePermission();
     if (status != Qt::PermissionStatus::Granted) {
         Permission::GetInstance()->RequestMicrophonePermission([this](const QPermission &permission) {
@@ -31,6 +37,7 @@ AudioCaputer::~AudioCaputer() {
         Stop();
         running_ = false;
     }
+    audioProbe_->stop();
 }
 
 void AudioCaputer::Init() {
@@ -42,38 +49,82 @@ void AudioCaputer::Init() {
         if (availableAudio == QMediaDevices::defaultAudioInput())
             audioDevice = availableAudio;
     }
-    audioInput_.reset(new QAudioInput());
-    audioInput_->setDevice(audioDevice);
     
-    audioRecorder_.reset(new QMediaRecorder);
-    captureSession_.setRecorder(audioRecorder_.data());
-    captureSession_.setAudioInput(audioInput_.data());
+    QAudioFormat format;
+    format.setSampleRate(44100);
+    format.setSampleFormat(QAudioFormat::Int16);
+    format.setChannelCount(2);
+    audioSource_.reset(new QAudioSource(audioDevice, format));
+    audioProbe_.reset(new AudioProbeDevice(this));
+    audioProbe_->start(audioSource_.data());
+    connect(audioProbe_.get(), &AudioProbeDevice::OnBufferChanged, this, [this](const QAudioBuffer &buffer) {
+        QAudioFormat format = buffer.format();
+        qDebug("sampleRate:%d, channelCount:%d, sampleFormat:%d. "
+               "SampleCount:%lld, FrameCount:%lld, ByteCount:%lld",
+               format.sampleRate(), format.channelCount(), format.sampleFormat(),
+               buffer.sampleCount(), buffer.frameCount(), buffer.byteCount());
+        //ffplay -ar 44100 -ac 2 -f s16le -i record.pcm
+        file_->write(buffer.constData<char>(), buffer.byteCount());
+    });
+    connect(audioProbe_.get(), &AudioProbeDevice::OnLevelChanged, this, [](const float level) {
+        qDebug() << "OnLevelChanged:" << level;
+    });
     
-    QMediaFormat format;
-    format.setAudioCodec(QMediaFormat::AudioCodec::MP3);
-    format.setFileFormat(QMediaFormat::MP3);
-    audioRecorder_->setMediaFormat(format);
-
-    audioRecorder_->setAudioSampleRate(44100); //采样频率。8kHz(电话)、44.1kHz(CD)、48kHz(DVD)。
-    audioRecorder_->setAudioChannelCount(1); //通道个数。常见的音频有立体声(stereo)和单声道(mono)两种类型，立体声包含左声道和右声道。另外还有环绕立体声等其它不太常用的类型。
-    audioRecorder_->setAudioBitRate(64000); // 32000, 64000, 96000, 128000
+//    audioInput_.reset(new QAudioInput());
+//    audioInput_->setDevice(audioDevice);
+//    audioRecorder_.reset(new QMediaRecorder);
+//    captureSession_.setRecorder(audioRecorder_.data());
+//    captureSession_.setAudioInput(audioInput_.data());
+//
+//    QMediaFormat format;
+//    format.setAudioCodec(QMediaFormat::AudioCodec::MP3);
+//    format.setFileFormat(QMediaFormat::MP3);
+//    audioRecorder_->setMediaFormat(format);
+//
+//    audioRecorder_->setAudioSampleRate(44100); //采样频率。8kHz(电话)、44.1kHz(CD)、48kHz(DVD)。
+//    audioRecorder_->setAudioChannelCount(1); //通道个数。常见的音频有立体声(stereo)和单声道(mono)两种类型，立体声包含左声道和右声道。另外还有环绕立体声等其它不太常用的类型。
+//    audioRecorder_->setAudioBitRate(64000); // 32000, 64000, 96000, 128000
     
     initialize_ = true;
 }
 
 void AudioCaputer::Start() {
     qDebug() << "AudioCaputer::Start.";
-    QString fileName = QFileDialog::getSaveFileName();
-    QUrl url = QUrl::fromLocalFile(fileName);
-    qDebug() << "fileName: " << fileName;
-    audioRecorder_->setOutputLocation(url);
-    audioRecorder_->record();
+//    QString fileName = QFileDialog::getSaveFileName();
+//    QUrl url = QUrl::fromLocalFile(fileName);
+//    qDebug() << "fileName: " << fileName;
+//    audioRecorder_->setOutputLocation(url);
+//    audioRecorder_->record();
+    
+    file_.reset(new QFile(*filePath_.get()));
+    if (!file_->open(QIODevice::WriteOnly)) {
+        qDebug() << "写文件失败！";
+    }
+    
+    auto *io = audioSource_->start();
+    if (!io) {
+        qDebug() << "AudioCaputer::Start Error! " << io;
+        return;
+    }
+    
+    connect(io, &QIODevice::readyRead, [this, io]() {
+        static const qint64 BufferSize = 4096;
+        const qint64 len = qMin(audioSource_->bytesAvailable(), BufferSize);
+        QByteArray buffer(len, 0);
+        qint64 l = io->read(buffer.data(), len);
+        if (l > 0) {
+            audioProbe_->writeData(buffer.data(), l);
+        }
+    });
+    
     running_ = true;
 }
 
 void AudioCaputer::Stop() {
     qDebug() << "AudioCaputer::Stop.";
-    audioRecorder_->stop();
+//    audioRecorder_->stop();
+    audioSource_->stop();
+    file_->close();
     running_ = false;
 }
 
--- QTDemo/src/multimedia/audio_capture.h
+++ QTDemo/src/multimedia/audio_capture.h
@@ -13,8 +13,61 @@
 #include <QMediaCaptureSession>
 #include <QMediaDevices>
 #include <QAudioSink>
+#include <QMediaFormat>
+#include <QAudioSource>
 #include <QMediaRecorder>
 #include <QScopedPointer>
+#include <QAudioBuffer>
+#include <QFile>
+
+class AudioProbeDevice : public QIODevice {
+    Q_OBJECT
+    QAudioFormat m_format;
+public:
+    AudioProbeDevice(QObject* parent = {}) : QIODevice(parent) {}
+    void start(QAudioSource *source) {
+        Q_ASSERT(source);
+        m_format = source->format();
+        open(QIODevice::WriteOnly);
+    }
+    
+    void stop() {
+        close();
+    }
+    
+    qreal calculateLevel(const char *data, qint64 len) {
+        const int channelBytes = m_format.bytesPerSample();
+        const int sampleBytes = m_format.bytesPerFrame();
+        const int numSamples = len / sampleBytes;
+
+        float maxValue = 0;
+        auto *ptr = reinterpret_cast<const unsigned char *>(data);
+
+        for (int i = 0; i < numSamples; ++i) {
+            for (int j = 0; j < m_format.channelCount(); ++j) {
+                float value = m_format.normalizedSampleValue(ptr);
+
+                maxValue = qMax(value, maxValue);
+                ptr += channelBytes;
+            }
+        }
+        emit OnLevelChanged(maxValue);
+        return maxValue;
+    }
+    
+    qint64 readData(char *, qint64) override { return 0; }
+    qint64 writeData(const char *data, qint64 count) override {
+        qDebug() << "writeData: " << count;
+        QAudioBuffer buffer({data, static_cast<int>(count)}, m_format);
+        emit OnBufferChanged(buffer);
+        calculateLevel(data, count);
+        return count;
+    }
+    
+    Q_SIGNAL void OnBufferChanged(const QAudioBuffer &buffer);
+    Q_SIGNAL void OnLevelChanged(const float level);
+};
+
 
 class AudioCaputer : public QObject {
     Q_OBJECT
@@ -29,9 +82,14 @@ public:
     bool IsInit() const { return initialize_; };
 
 private:
-    QMediaCaptureSession captureSession_;
-    QScopedPointer<QAudioInput> audioInput_;
-    QScopedPointer<QMediaRecorder> audioRecorder_;
+//    QMediaCaptureSession captureSession_;
+//    QScopedPointer<QAudioInput> audioInput_;
+//    QScopedPointer<QMediaRecorder> audioRecorder_;
+    
+    QScopedPointer<QAudioSource> audioSource_;
+    QScopedPointer<AudioProbeDevice> audioProbe_;
+    QScopedPointer<QFile> file_;
+    QScopedPointer<QString> filePath_;
     
     bool running_ = false;
     bool initialize_ = false;
